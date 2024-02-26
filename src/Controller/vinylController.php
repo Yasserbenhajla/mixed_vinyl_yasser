21c31123bc33d87f61ab31892a72be21f7e6df02
Yasser Ben Hajla <yaserbenhajla@gmail.com>
2024-02-26 15:22:54 +0100

    "Added some Tiwggy goodness"

--- src/Controller/vinylController.php
+++ src/Controller/vinylController.php
@@ -1,16 +1,29 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Storage\Proxy\AbstractProxy;
 use Symfony\Component\Routing\Annotation\Route;
 use function  Symfony\Component\String\u ;
 
-class vinylController
+class vinylController extends AbstractController 
 {
     #[Route("/")]
     function homepage() : Response
     {
-        return new Response ("bonjour <strong>DSI22 G1 </strong>");
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return $this->render ('vinyl/homepage.html.twig',[
+            'title'=>'ISET kélibia',
+            'tracks'=> $tracks 
+        ]);
     }
     #[Route("/browse/{slug}")]
     function browse(string $slug = null) : Response
@@ -24,5 +37,3 @@ class vinylController
             return new Response ($title);
     }
 }
-
-?> 
