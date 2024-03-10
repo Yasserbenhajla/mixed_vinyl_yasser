27440e6f63f26ea05b4ce5118c3934aef92a021e
Yasser Ben Hajla <yaserbenhajla@gmail.com>
2024-03-10 23:23:34 +0100

    "Never gonna let you go..."

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id}',methods: ['GET'])]
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
     public function getSong(int $id,LoggerInterface $logger): Response
     {
 
--- src/Controller/vinylController.php
+++ src/Controller/vinylController.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
+use function  Symfony\Component\String\u ;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Storage\Proxy\AbstractProxy;
 use Symfony\Component\Routing\Annotation\Route;
 
-use function  Symfony\Component\String\u ;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Storage\Proxy\AbstractProxy;
 
 class vinylController extends AbstractController 
 {
